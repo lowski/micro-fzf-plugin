5641ae1532ea13a653f0b6024a6c113f436d075c
Lucas Mendelowski <lucas@mendelowski.com>
2021-03-04 18:07:23 +0100

    WIP

--- main.lua
+++ main.lua
@@ -1,29 +1,23 @@
-VERSION = "1.1.1"
+VERSION = "2.0.0"
 
 local micro = import("micro")
 local shell = import("micro/shell")
 local config = import("micro/config")
 local buffer = import("micro/buffer")
+local strings = import("strings")
 
 function fzf(bp)
-    if shell.TermEmuSupported then
-        local err = shell.RunTermEmulator(bp, "fzf", false, true, fzfOutput, {bp})
-        if err ~= nil then
-            micro.InfoBar():Error(err)
-        end
+    local command = [[fzf --info inline --preview 'cat {}']]
+    local output, err = shell.RunInteractiveShell(command, false, true)
+    if err ~= nil then
+        micro.InfoBar():Error(err)
     else
-        local output, err = shell.RunInteractiveShell("fzf", false, true)
-        if err ~= nil then
-            micro.InfoBar():Error(err)
-        else
-            fzfOutput(output, {bp})
-        end
+        fzfOutput(output, {bp})
     end
 end
 
 function fzfOutput(output, args)
     local bp = args[1]
-    local strings = import("strings")
     output = strings.TrimSpace(output)
     if output ~= "" then
         local buf, err = buffer.NewBufferFromFile(output)
